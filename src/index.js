3ad844b3cceb39f91c77147048bcdef8570b9e04
Abraomukas <abraomukas@gmail.com>
2024-01-03 11:10:20 +0000

    feat: Add Error page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const spinnerSizing = { width: '3rem', height: '3rem' };
+
+const spinnerFallback = (
+	<div className='d-flex justify-content-center mt-3'>
+		<div className='spinner-border' style={spinnerSizing} role='status'>
+			<span className='visually-hidden'>Loading...</span>
+		</div>
+	</div>
+);
+
+/**
+ * PAGES
+ */
+
+import ErrorPage from './pages/Error';
+import Main from './pages/Main';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Main />,
+		errorElement: <ErrorPage />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.Suspense fallback={spinnerFallback}>
+		<RouterProvider router={router} />
+	</React.Suspense>
 );
