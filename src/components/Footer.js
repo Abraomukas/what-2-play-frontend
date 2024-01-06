bf306c3f9266d516e560292c11507c96e92466ac
Abraomukas <abraomukas@gmail.com>
2024-01-06 20:10:20 +0000

    feat: Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
 	return (
-		<div style={{ position: 'relative', minHeight: '100vh' }}>
-			<div>FOOTER</div>
-		</div>
+		<nav className='navbar navbar-expand-lg navbar-light bg-light h-100 d-flex justify-content-between align-items-center'>
+			{/* GRID CONTAINER */}
+			<div className='container p-4'>
+				{/* SOCIAL MEDIA */}
+				<section className='mb-4'>
+					<div className='btn-group' role='group'>
+						{/* INSTAGRAM */}
+						<Link to='https://www.instagram.com/abraomukas' target='_blank'>
+							<button
+								type='button'
+								className='btn btn-outline-primary'
+								style={{ backgroundColor: '#3b5998' }}>
+								<i className='fab fa-instagram text-white'></i>
+							</button>
+						</Link>
+
+						{/* TWITTER */}
+						<Link to='https://twitter.com/abraham_g11' target='_blank'>
+							<button
+								type='button'
+								className='btn btn-outline-primary'
+								style={{ backgroundColor: '#3b5998' }}>
+								<i className='fas fa-x text-white'></i>
+							</button>
+						</Link>
+					</div>
+				</section>
+				{/* TEXT */}
+				<section className='mb-4'>
+					<div>
+						{/* GRID ROW */}
+						<div className='row d-flex justify-content-center'>
+							{/* GRID COLUMN */}
+							<div className='col-auto'>
+								<p>
+									<strong>What 2 Play</strong>
+									<small> by Abraomukas</small>
+								</p>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</nav>
 	);
 }
 
