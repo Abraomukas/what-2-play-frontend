f1fee21e4ea95d4f6c78b521707df2fde1504365
Abraomukas <abraomukas@gmail.com>
2024-01-15 22:00:20 +0100

    feat: Games section stub

---
+++ src/pages/Games.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//* COMPONENTS
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+function Games() {
+	const games = [];
+
+	return (
+		<div>
+			<Navbar />
+			{/* CAROUSEL */}
+			<Footer />
+		</div>
+	);
+}
+
+export default Games;
