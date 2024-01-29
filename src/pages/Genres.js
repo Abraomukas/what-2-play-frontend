7fdc4f5639a7609e2afad81a244ed64551336e54
Abraomukas <abraomukas@gmail.com>
2024-01-29 01:40:50 +0100

    feat: Render genres based on input array

--- src/pages/Genres.js
+++ src/pages/Genres.js
@@ -7,21 +7,21 @@ import GameBox from '../components/GameBox';
 
 const Genres = () => {
 	const genres = [
-		{ id: 1, title: 'RPG' },
-		{ id: 2, title: 'Driving' },
-		{ id: 3, title: 'Sports' },
-		{ id: 4, title: 'Shooter' },
-		{ id: 5, title: 'Indie' },
-		{ id: 6, title: 'Fighting' },
-		{ id: 7, title: 'Horror' },
-		{ id: 8, title: 'Simulator' },
-		{ id: 9, title: 'Souls-like' },
-		{ id: 10, title: 'Action - Adventure' },
-		{ id: 11, title: 'MMO' },
-		{ id: 12, title: 'Coop' },
-		{ id: 13, title: 'Battle Royale' },
-		{ id: 14, title: 'Puzzle' },
-		{ id: 15, title: 'Platformer' },
+		{ title: 'RPG' },
+		{ title: 'Driving' },
+		{ title: 'Sports' },
+		{ title: 'Shooter' },
+		{ title: 'Indie' },
+		{ title: 'Fighting' },
+		{ title: 'Horror' },
+		{ title: 'Simulator' },
+		{ title: 'Souls-like' },
+		{ title: 'Action - Adventure' },
+		{ title: 'MMO' },
+		{ title: 'Coop' },
+		{ title: 'Battle Royale' },
+		{ title: 'Puzzle' },
+		{ title: 'Platformer' },
 	];
 
 	return (
@@ -32,58 +32,24 @@ const Genres = () => {
 				style={{ position: 'relative', minHeight: '100vh' }}
 				className='d-flex justify-content-between align-items-center'>
 				<div className='container text-center'>
-					<div class='row row-cols-5 row-cols-md-5 g-4'>
-						<div class='col'>
-							<div class='card'>
-								<img src='...' class='card-img-top' alt='...' />
-								<div class='card-body'>
-									<h5 class='card-title'>Card title</h5>
-									<p class='card-text'>
-										This is a longer card with supporting text below as a
-										natural lead-in to additional content. This content is a
-										little bit longer.
-									</p>
+					<div className='row row-cols-5 row-cols-md-5 g-4'>
+						{genres.map(({ title }, index) => {
+							return (
+								<div key={index} className='col'>
+									<div className='card'>
+										<img
+											src='...'
+											className='card-img-top'
+											alt={`${title} games`}
+										/>
+										<div className='card-body'>
+											<h5 className='card-title'>{title}</h5>
+											<p className='card-text'>Lore ipsum.</p>
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
-						<div class='col'>
-							<div class='card'>
-								<img src='...' class='card-img-top' alt='...' />
-								<div class='card-body'>
-									<h5 class='card-title'>Card title</h5>
-									<p class='card-text'>
-										This is a longer card with supporting text below as a
-										natural lead-in to additional content. This content is a
-										little bit longer.
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class='col'>
-							<div class='card'>
-								<img src='...' class='card-img-top' alt='...' />
-								<div class='card-body'>
-									<h5 class='card-title'>Card title</h5>
-									<p class='card-text'>
-										This is a longer card with supporting text below as a
-										natural lead-in to additional content.
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class='col'>
-							<div class='card'>
-								<img src='...' class='card-img-top' alt='...' />
-								<div class='card-body'>
-									<h5 class='card-title'>Card title</h5>
-									<p class='card-text'>
-										This is a longer card with supporting text below as a
-										natural lead-in to additional content. This content is a
-										little bit longer.
-									</p>
-								</div>
-							</div>
-						</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
