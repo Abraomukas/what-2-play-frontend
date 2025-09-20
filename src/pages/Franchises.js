1cc0afabdfe08cfde88aa603918ab4382acaa3c4
Abraomukas <abraomukas@gmail.com>
2025-09-20 23:00:04 +0200

    feat: updated layout for Franchises

--- src/pages/Franchises.js
+++ src/pages/Franchises.js
@@ -1,74 +1,49 @@
-import React from 'react';
+import React from "react";
 
 //* COMPONENTS
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Franchises() {
-	const games = [];
+  const franchises = [
+    { title: "Final Fantasy", picture: "final-fantasy-xvi.webp", path: "" },
+    { title: "Need for Speed", picture: "nfs-unbound.webp", path: "" },
+  ];
 
-	return (
-		<div>
-			<Navbar />
-			<div
-				style={{ position: 'relative', minHeight: '100vh' }}
-				className='d-flex justify-content-between align-items-center'>
-				<div className='container text-center'>
-					<div>FRANCHISES</div>
+  return (
+    <div>
+      <Navbar />
 
-					{/* CAROUSEL */}
-					<div
-						id='carouselExampleFade'
-						className='carousel slide carousel-fade'>
-						<div className='carousel-inner'>
-							<div className='carousel-item active'>
-								<img
-									src='./images/returnal.webp'
-									className='d-block w-100'
-									alt='...'
-								/>
-							</div>
-							<div className='carousel-item'>
-								<img
-									src='./images/profile.jpg'
-									className='d-block w-100'
-									alt='...'
-								/>
-							</div>
-							<div className='carousel-item'>
-								<img
-									src='./playstation.svg'
-									className='d-block w-100'
-									alt='...'
-								/>
-							</div>
-						</div>
-						<button
-							className='carousel-control-prev'
-							type='button'
-							data-bs-target='#carouselExampleFade'
-							data-bs-slide='prev'>
-							<span
-								className='carousel-control-prev-icon'
-								aria-hidden='true'></span>
-							<span className='visually-hidden'>Previous</span>
-						</button>
-						<button
-							className='carousel-control-next'
-							type='button'
-							data-bs-target='#carouselExampleFade'
-							data-bs-slide='next'>
-							<span
-								className='carousel-control-next-icon'
-								aria-hidden='true'></span>
-							<span className='visually-hidden'>Next</span>
-						</button>
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+      <div
+        style={{ position: "relative", minHeight: "100vh" }}
+        className="d-flex justify-content-between align-items-center my-5"
+      >
+        {/* GRID */}
+        <div className="container text-center">
+          <div className="row row-cols-5 row-cols-md-5 g-4">
+            {franchises.map(({ title, picture }, index) => {
+              return (
+                <div key={index} className="col">
+                  <div className="card">
+                    <img
+                      src={`./images/${picture}`}
+                      className="card-img-top"
+                      alt={`${title} games`}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{title}</h5>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Franchises;
